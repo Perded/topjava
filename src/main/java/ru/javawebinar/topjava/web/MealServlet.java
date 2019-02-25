31aae037a3ff54d677cd9c422ae9a548cc5c3fa2
KuzmichevVB <echo_vk1@yahoo.com>
2019-02-25 11:39:26 +0900

    HW01 list of meal

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -15,9 +18,8 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
-
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("meals", MealsUtil.getFilteredWithExcess(MealsUtil.meals, LocalTime.of(0, 0), LocalTime.of(23, 59), 2000));
+        log.debug("forward to meals");
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,9 +1,25 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <head>
     <title>Meals</title>
 </head>
 <body>
+<table>
+    <tr>
+        <th>DateTime</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <c:forEach items="${meals}" var="meal">
+        <tr style="color:
+            <c:out value="${meal.isExcess() ? 'red' : 'green'}"/>">
+            <td>${meal.getDateTime().toString().replace("T", " ")}</td>
+            <td>${meal.getDescription()}</td>
+            <td>${meal.getCalories()}</td>
+        </tr>
+    </c:forEach>
+</table>
 <h3><a href="index.html">Home</a></h3>
 <h2>Meals</h2>
 </body>
